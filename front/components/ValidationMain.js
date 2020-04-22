861fc7b92bfc6b21c22168fd937a032d103bbc54
Julien <julien.vignoud@epfl.ch>
2020-04-22 16:07:08 +0200

    Still 500

--- front/components/ValidationMain.js
+++ front/components/ValidationMain.js
@@ -47,56 +47,54 @@ export default class ImagePickerExample extends React.Component {
       if (!result.cancelled) {
         this.setState({ image: result.uri });
       }
-
-      console.log(result);
+      // console.log(result);
     } catch (E) {
       console.log(E);
     }
   };
 
  _upload = async () => {
-  if(this.state.image == null){
-    console.log(this.props.challengeId)
-    return
+    // if(this.state.image == null){
+    //   console.log(this.props.challengeId)
+    //   return
+    // }
+    // let uriParts = this.state.image.split('.');
+    // let fileType = uriParts[uriParts.length - 1];
+
+    // let formData = new FormData();
+    // formData.append('photo', {
+    //   uri,
+    //   name: `photo.${fileType}`,
+    //   type: `image/${fileType}`,
+    // });
+
+    let bodyFormData = new FormData();
+    bodyFormData.append('challengeId', this.props.challengeId);
+    bodyFormData.append('commentary',"Commentaire constructif"); //this.review
+    bodyFormData.append('picture',""); //this.state.image
+
+    request({
+      method: 'post',
+      url : '/api/completeMyChallenge',
+      data : bodyFormData,
+      headers: {'Content-Type':'multipart/form-data'}
+    }).then(function(response){
+    }).catch(function(error){
+      console.log("Got here !")
+      console.log(error.response)
+    })
+
+    // let options = {
+    //   method: 'POST',
+    //   body: formData,
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'multipart/form-data',
+    //   },
+    // };
+
+    // return fetch(apiUrl, options);
+    // }
+
   }
-
-  let uriParts = this.state.image.split('.');
-  let fileType = uriParts[uriParts.length - 1];
-
-  // let formData = new FormData();
-  // formData.append('photo', {
-  //   uri,
-  //   name: `photo.${fileType}`,
-  //   type: `image/${fileType}`,
-  // });
-
-  console.log("Got here !")
-  let bodyFormData = new FormData();
-  bodyFormData.append('challengeId', this.props.challengeId);
-  bodyFormData.append('commentary',this.review);
-  bodyFormData.append('picture',""); //à changer
-
-  return request({
-    method: 'post',
-    url : '/api/completeMyChallenge',
-    data : bodyFormData,
-    headers: {'Content-Type':'multipart/form-data'}
-  }).then(function(response){
-  }).catch(function(error){
-    console.log(error)
-  })
-
-  // let options = {
-  //   method: 'POST',
-  //   body: formData,
-  //   headers: {
-  //     Accept: 'application/json',
-  //     'Content-Type': 'multipart/form-data',
-  //   },
-  // };
-
-  // return fetch(apiUrl, options);
-  // }
-
-}
 }
