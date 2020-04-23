f66f18ea5ce9c13a7d5e1065ce0b34fdeb1cb521
Julien <julien.vignoud@epfl.ch>
2020-04-23 18:49:22 +0200

    Username now displayed next to comments #56

--- front/components/Screens/ChallengeDetailScreen.vue
+++ front/components/Screens/ChallengeDetailScreen.vue
@@ -32,29 +32,8 @@
       <view class="comments-container">
         <view class="comments" v-for="(commentaire, index) in commentaires" :key="index">
           <view class="comment-container">
-            <text class="comment">{{commentaire}}</text>
-          </view>
-          <view class="comment-container">
-            <text class="comment">Vous savez, moi je ne crois pas qu'il y ait de bonnes 
-              ou de mauvaises situations. Moi si je devais résumer ma vie, aujourd'hui, 
-              avec vous, je dirais que c'est d'abord des rencontres, des gens qui m'ont 
-              tendu la main, peut-être à un moment où je ne pouvais pas, où j'étais seul 
-              chez moi, et c'est assez curieux de se dire que les hasards, les rencontres 
-              forgent une destinée, parce que quand on a le goût de la chose, quand on a 
-              le goût de la chose bien faite, le beau geste, parfois on ne trouve pas 
-              l'interlocuteur en face, je dirais le miroir qui vous aide à avancer ; alors 
-              ce n'est pas mon cas comme je le disais là, puisque moi au contraire j'ai pu 
-              et je dis merci à la vie, je lui dis merci, je chante la vie, je danse la vie, 
-              je ne suis qu'amour, et finalement quand beaucoup de gens aujourd'hui me disent : 
-              Mais comment fais-tu pour avoir cette humanité ? Et bah je leur réponds très 
-              simplement, je leur dis : c'est ce goût de l'amour, ce goût donc qui m'a 
-              poussé, aujourd'hui, à entreprendre une construction mécanique mais demain, 
-              qui sait, peut-être, simplement à me mettre au service de la communauté, à 
-              faire le don, le don de soi...
-            </text>
-          </view>
-          <view class="comment-container">
-            <text class="comment">Sa va?</text>
+            <text class="user">{{commentaire[0]}}</text>
+            <text class="comment">{{commentaire[1]}}</text>
           </view>
         </view>
       </view>
@@ -186,11 +165,15 @@
   padding:30;
   
 }
-
+.user{
+  font-size: 20;
+  color : #3d9d84;
+  margin-bottom:5;
+}
 .comment{
   color :#4e4e4e;
   font-size: 20;
-
+  /* padding-left:30; */
 }
 
 </style>
