7ff50c50c200de4881e2507d506d17755fe6036a
Elisa Michelet <elisa.michelet@epfl.ch>
2020-04-23 11:59:16 +0200

    some more import

--- back/src/main/java/coronhackathon/backend/repository/CompletedRepository.java
+++ back/src/main/java/coronhackathon/backend/repository/CompletedRepository.java
@@ -2,6 +2,7 @@ package coronhackathon.backend.repository;
 
 import coronhackathon.backend.entity.Challenge;
 import coronhackathon.backend.entity.HasCompleted;
+import coronhackathon.backend.entity.HasCompletedId;
 import coronhackathon.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
